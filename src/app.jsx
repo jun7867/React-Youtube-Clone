ab7351cda9b3b76641427d181aa2bfca297b68b8
남쭌 <jjune095@naver.com>
2020-10-11 21:33:50 +0900

    youtube init

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./app.css";
+import VideoList from "./components/videoList"
+function App() {
+  return (<>
+    <VideoList />
+  </>);
+}
+
+export default App;
