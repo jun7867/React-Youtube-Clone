4d01ef364dc55aac992a3fa916c2d1f58c6c045f
남쭌 <jjune095@naver.com>
2020-10-12 00:43:03 +0900

    video component css

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import styles from './video_item.module.css'
 
-const VideoItem = (props) => {
+// props.video.snippet ~~ 인데 아래 방식으로 {video: {snippet}}으로 사용 가능.
+const VideoItem = ({video: {snippet}}) => {
     return (
-        <h2>
-            {props.video.snippet.title}
-        </h2>
+        <li className={styles.container}>
+            <div className={styles.video}>
+                <img  
+                    className={styles.thumbnail} 
+                    src={snippet.thumbnails.medium.url} 
+                    alt="video thumbnail">       
+                </img>
+                <div className={styles.metadata}>
+                    <p className={styles.title} >{snippet.title}</p>
+                    <p className={styles.channel} >{snippet.channelTitle}</p>
+                </div>
+            </div>
+        </li>
     );
 }
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import VideoItem from '../video_item/video_item'
+import styles from './video_list.module.css'
 
 const VideoList = (props) => {
     return (
-    <ul> 
+    <ul className={styles.videos}> 
         {props.videos.map(video =>
          (<VideoItem key={video.id} video={video} />
         ))}
