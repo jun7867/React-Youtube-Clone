a3bd2cf14dd96a1cae863f817417f2026c8bfa1c
남쭌 <jjune095@naver.com>
2020-10-13 04:03:01 +0900

    seach implement

--- src/app.jsx
+++ src/app.jsx
@@ -18,11 +18,24 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   },[])  // [] 으로 하면 처음에만 호출
-
+  
+  const handleSearch = (keyword) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${keyword}&type=video&key=AIzaSyD246ZkNrfVgVPpNrs0OoFtX4gbNmz7DrQ`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId}))) // 이 과정을 하는 이유는 search API에서 video id가 object로 한단계 더 들어가야 되기 때문.
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   return (
     <div className={styles.app}>
-    <Search />
+    <Search 
+      onSearch={handleSearch}/>
     <VideoList videos={videos}/>
   </div>);
 }
--- src/components/video_search/video_search.jsx
+++ src/components/video_search/video_search.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { useRef } from 'react';
+import { useState ,useEffect} from 'react';
 import styles from './video_search.module.css';
 
 const VideoSearch = (props) => {
+    const inputRef = useRef();
+    const btnRef = React.createRef();
+    
+    const handleSearch = () => {
+        const keyword=inputRef.current.value;
+        keyword && props.onSearch(keyword);
+    };
+    const onClick = (event) => {
+        handleSearch();
+    }
+
+    const onKeyPress = (event) => {
+        if (event.key ==='Enter') {
+            handleSearch();
+        }
+    }
+
     return (
         <header className={styles.box}>
             <img className={styles.logo} src='/images/logo.png' alt='logo'></img>
             <span className={styles.name}>Youtube</span>
-            <input className={styles.input} type='search' placeholder="Search.."></input>
-            <button className={styles.searchBtn} type='submit'>
+            <input className={styles.input} type='search' ref={inputRef} placeholder="Search.." onKeyPress={onKeyPress}></input>
+            <button ref={btnRef} className={styles.searchBtn} type='submit' onClick={onClick} >
                 <img className={styles.search} src="/images/search.png" alt='search'></img>
-            </button>        
+            </button>      
         </header>
+
     );
 }
 
