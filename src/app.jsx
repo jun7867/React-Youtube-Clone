ce0bda21f807e72f787f026f4bc7d6da3173d282
남쭌 <jjune095@naver.com>
2020-10-15 18:13:54 +0900

    back to mainPage when you  click logo, search

--- src/app.jsx
+++ src/app.jsx
@@ -1,46 +1,55 @@
 import React from "react";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import styles from "./app.module.css";
-import VideoDetail from './components/video_detail/video_detail';
-import VideoList from "./components/video_list/video_list"
-import Search from './components/video_search/video_search'
+import VideoDetail from "./components/video_detail/video_detail";
+import VideoList from "./components/video_list/video_list";
+import Search from "./components/video_search/video_search";
 
-function App( {youtube}) {
-  const [videos,setVideos]=useState([]);
-  const [selectedVideo,setSelectedVideo]=useState(null);
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const logoClick = () => {
+    setSelectedVideo(null);
+    youtube.mostPopular().then((videos) => setVideos(videos));
+  };
+
   //비디오가 클릭이 되면 selectVideo 호출되어 selectedVideo에 클릭한 비디오가 저장됨.
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
 
-  const search = query => {
-    youtube.search(query)
-    .then(videos=> setVideos(videos));
+  const search = (query) => {
+    setSelectedVideo(null);
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
-  useEffect( () => {
-    youtube.mostPopular()
-    .then(videos=> setVideos(videos))
-  },[])  // [] 으로 하면 처음에만 호출
-
+  useEffect(() => {
+    youtube.mostPopular().then((videos) => setVideos(videos));
+  }, []); // [] 으로 하면 처음에만 호출
 
   return (
     <div className={styles.app}>
-    <Search 
-      onSearch={search}/>
-    
-    <section className={styles.content}>
-      {selectedVideo && (
-        <div className={styles.detail}>
-          <VideoDetail video={selectedVideo}/> 
-        </div>
+      <Search onSearch={search} onLogoClick={logoClick} />
+
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
         )}
-      
+
         <div className={styles.list}>
-          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
         </div>
-    </section>
-    
-  </div>);
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- src/components/video_search/video_search.jsx
+++ src/components/video_search/video_search.jsx
@@ -1,36 +1,57 @@
-import React from 'react';
-import { useRef } from 'react';
-import styles from './video_search.module.css';
+import React from "react";
+import { useRef } from "react";
+import styles from "./video_search.module.css";
 
-const VideoSearch = (props) => {
-    const inputRef = useRef();
-    const btnRef = React.createRef();
-    
-    const handleSearch = () => {
-        const keyword=inputRef.current.value;
-        keyword && props.onSearch(keyword);
-    };
-    const onClick = (event) => {
-        handleSearch();
+const VideoSearch = ({ onSearch, onLogoClick }) => {
+  const inputRef = useRef();
+  const btnRef = React.createRef();
+
+  const handleSearch = () => {
+    const keyword = inputRef.current.value;
+    keyword && onSearch(keyword);
+  };
+  const onClick = (event) => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
     }
+  };
 
-    const onKeyPress = (event) => {
-        if (event.key ==='Enter') {
-            handleSearch();
-        }
-    }
-
-    return (
-        <header className={styles.box}>
-            <img className={styles.logo} src='/images/logo.png' alt='logo'></img>
-            <span className={styles.name}>Youtube</span>
-            <input className={styles.input} type='search' ref={inputRef} placeholder="Search.." onKeyPress={onKeyPress}></input>
-            <button ref={btnRef} className={styles.searchBtn} type='submit' onClick={onClick} >
-                <img className={styles.search} src="/images/search.png" alt='search'></img>
-            </button>      
-        </header>
-
-    );
-}
+  return (
+    <header className={styles.box}>
+      <img
+        className={styles.logo}
+        src="/images/logo.png"
+        alt="logo"
+        onClick={onLogoClick}
+      ></img>
+      <span className={styles.name} onClick={onLogoClick}>
+        Youtube
+      </span>
+      <input
+        className={styles.input}
+        type="search"
+        ref={inputRef}
+        placeholder="Search.."
+        onKeyPress={onKeyPress}
+      ></input>
+      <button
+        ref={btnRef}
+        className={styles.searchBtn}
+        type="submit"
+        onClick={onClick}
+      >
+        <img
+          className={styles.search}
+          src="/images/search.png"
+          alt="search"
+        ></img>
+      </button>
+    </header>
+  );
+};
 
 export default VideoSearch;
